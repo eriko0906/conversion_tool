1da24b885e8addb17aa3030744f707fcbc42abf3
eripi0906 <per.music.ef.2@gmail.com>
2023-07-02 13:59:57 +0900

    vmコード生成機能追加

--- output.tf
+++ output.tf
@@ -1,21 +1,20 @@
 #----------------------------------------------------------
 #  プロバイダ設定
-#  プロバイダ：aws
+#  プロバイダ：google
 #  リージョン：asia-northeast1
 #  プロジェクトID(GCPの時のみ)：nttd-platformtec
 #----------------------------------------------------------
-provider "aws" {
+provider "google" {
   region   = "asia-northeast1"
+  project = "nttd-platformtec"
 }
 
 #----------------------------------------------------------
 #  VPC設定
 #  VPC名：IA-fujitaerb-vm
 #----------------------------------------------------------
-resource "aws_vpc" "IA-fujitaerb-vm" {
-  tags = {
-    Name = "IA-fujitaerb-vm"
-  }
+resource "google_compute_network" "IA-fujitaerb-vm" {
+  name   = "IA-fujitaerb-vm"
 }
 
 #----------------------------------------------------------
@@ -23,11 +22,28 @@ resource "aws_vpc" "IA-fujitaerb-vm" {
 #  サブネット名：main
 #  CIDR範囲：10.0.1.0/24
 #----------------------------------------------------------
-resource "aws_subnet" "main" {
-  vpc_id   = aws_vpc.IA-fujitaerb-vm.id
-  cidr_block   = "10.0.1.0/24"
-  tags = {
-    Name = "main"
+resource "google_compute_subnetwork" "main" {
+  name   = "main"
+  network   = google_compute_network.IA-fujitaerb-vm.id
+  ip_cidr_range   = "10.0.1.0/24"
+}
+
+#----------------------------------------------------------
+#  VM設定
+#  インスタンス名：ia-fujitaerb-vm
+#  マシン・インスタンスタイプ：t2.micro
+#  マシンイメージ・ami：ami-0f903fb156f24adbf
+#----------------------------------------------------------
+resource "google_compute_instance" "ia-fujitaerb-vm" {
+  name   = "ia-fujitaerb-vm"
+  machine_tyep   = "t2.micro"
+  boot_disk {
+    initialize_params {
+      image = "ami-0f903fb156f24adbf"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.main.id
   }
 }
 
--- tool.rb
+++ tool.rb
@@ -48,6 +48,36 @@ def generate_subnet_code(subnet_name,cidr,vpc_name,provider_name)
   resource_code
 end
 
+def generate_vm_code(instance_name, machine_image, machine_type,subnet_name,provider_name)
+  if provider_name == "aws"
+    resource_code = 'resource "aws_instance" "' + instance_name + '" {' + "\n"
+    resource_code += '  ami   = "' + machine_image + '"' + "\n"
+    resource_code += '  instance_type   = "' + machine_type + '"' + "\n"
+    resource_code += '  subnet_id   = aws_subnet.' + subnet_name + '.id' + "\n"
+    resource_code += '  tags = {' + "\n"
+    resource_code += '    Name = "' + instance_name + '"' + "\n"
+    resource_code += '  }' + "\n"
+  end
+
+  if provider_name == "google"
+    resource_code = 'resource "google_compute_instance" "' + instance_name + '" {' + "\n"
+    resource_code += '  name   = "' + instance_name + '"' + "\n"
+    resource_code += '  machine_tyep   = "' + machine_type + '"' + "\n"
+    resource_code += '  boot_disk {' + "\n"
+    resource_code += '    initialize_params {' + "\n"
+    resource_code += '      image = "' + machine_image + '"' + "\n"
+    resource_code += '    }' + "\n"
+    resource_code += '  }' + "\n"
+    resource_code += '  network_interface {' + "\n"
+    resource_code += '    subnetwork = google_compute_subnetwork.' + subnet_name + '.id' + "\n"
+    resource_code += '  }' + "\n"
+  end
+
+  resource_code += '}' + "\n\n"
+
+  resource_code
+end
+
 def read_input(file_path)
   File.read(file_path)
 end
@@ -63,6 +93,9 @@ def parse_input(input)
   vpc_name = ""
   subnet_name = ""
   cidr = ""
+  instance_name  = ""
+  machine_type = ""
+  machine_image = ""
   
   input.lines.each do |line|
     line = line.strip
@@ -80,10 +113,16 @@ def parse_input(input)
       subnet_name = line.split("：")[1]
     elsif line.include?("CIDR範囲：")
       cidr = line.split("：")[1]
+    elsif line.include?("インスタンス名：")
+    instance_name = line.split("：")[1]
+    elsif line.include?("マシン・インスタンスタイプ：")
+    machine_type = line.split("：")[1]
+    elsif line.include?("マシンイメージ・ami：")
+    machine_image = line.split("：")[1]
     end
   end
 
-  [provider_name, project_id, region, vpc_name, subnet_name, cidr]
+  [provider_name, project_id, region, vpc_name, subnet_name, cidr, instance_name, machine_image, machine_type]
 end
 
 def main(input_file, output_file)
@@ -91,13 +130,14 @@ def main(input_file, output_file)
   line = "#----------------------------------------------------------\n"
   sections = input.split(line)
 
-  provider_name, project_id, region, vpc_name, subnet_name, cidr = parse_input(input)
+  provider_name, project_id, region, vpc_name, subnet_name, cidr, instance_name, machine_image, machine_type= parse_input(input)
   
   provider_code = generate_provider_code(provider_name, project_id, region)
   vpc_code = generate_vpc_code(vpc_name, provider_name)
   subnet_code = generate_subnet_code(subnet_name,cidr,vpc_name,provider_name)
+  vm_code = generate_vm_code(instance_name, machine_image, machine_type,subnet_name,provider_name)
   
-  terraform_code = line + sections[1] + line + provider_code + line + sections[3] + line + vpc_code + line + sections[5] + line +subnet_code
+  terraform_code = line + sections[1] + line + provider_code + line + sections[3] + line + vpc_code + line + sections[5] + line +subnet_code + line + sections[7] + line +vm_code
   write_output(output_file, terraform_code)
 end
 
